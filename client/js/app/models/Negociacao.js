c9cc18da8296ecf499e6f621b7a0c4c4d9640681
Renato Crudo <re.crudo.85@gmail.com>
2019-03-17 18:59:07 -0300

    A classe Negociação

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,9 @@
+class Negociacao {
+    //Cria as propriedades
+    //this => sempre aponta para a instancia que está sendo executada
+    constructor() {
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+}
