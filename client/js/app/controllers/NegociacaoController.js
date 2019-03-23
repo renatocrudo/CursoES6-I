9b8bc88392b99c4101aef371ed6e41da6a22fb6f
Renato Crudo <re.crudo.85@gmail.com>
2019-03-23 16:41:29 -0300

    Evitando percorrer o DOM muitas vezes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,21 +1,24 @@
 class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+/*
+O ideial é utilizar no construtor para não precisar ficar percorrendo o DOM toda a vez que clicar no botão de evento
+*/
+
     //adicionando um metodo
     adiciona(event) {
         event.preventDefault();
-        
-        //pegando os dados vindos da pagina
-        //outra opção para minimizar a escrita de codigo é criar um alias, muito parecido com o funcionamento do jquery
-        let $ = document.querySelector.bind(document); //o bind serve para não perder a referencia do this
-        //let inputData = document.querySelector('#data');
-        let inputData = $('#data');
-        //let inputQuantidade = document.querySelector('#quantidade');
-        let inputQuantidade = $('#quantidade');
-        //let inputValor = document.querySelector('#valor');
-        let inputValor = $('#valor');
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
 
     }
 }
