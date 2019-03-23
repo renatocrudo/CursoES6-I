c8ed0bf9bacd4f5e1b02b7ca75898fd222f09f21
Renato Crudo <re.crudo.85@gmail.com>
2019-03-23 16:31:43 -0300

    Associando métodos do controller às ações do user

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,20 @@ class NegociacaoController {
     //adicionando um metodo
     adiciona(event) {
         event.preventDefault();
-        alert('Chamei ação do controller');
+        
+        //pegando os dados vindos da pagina
+        //outra opção para minimizar a escrita de codigo é criar um alias, muito parecido com o funcionamento do jquery
+        let $ = document.querySelector.bind(document); //o bind serve para não perder a referencia do this
+        //let inputData = document.querySelector('#data');
+        let inputData = $('#data');
+        //let inputQuantidade = document.querySelector('#quantidade');
+        let inputQuantidade = $('#quantidade');
+        //let inputValor = document.querySelector('#valor');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
+
     }
 }
